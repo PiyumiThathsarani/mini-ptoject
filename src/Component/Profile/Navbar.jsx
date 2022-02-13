41d994e2db8eff0f3bfc59061900e6535b8ee1a0
Piyumi Thathsarani <ptwanniarachchi98@gmail.com>
2022-02-13 14:46:46 +0530

    Home page styled

--- src/Component/Profile/Navbar.jsx
+++ src/Component/Profile/Navbar.jsx
@@ -7,15 +7,15 @@ import Header from './Header';
 
 function Navbar() {
     return (
-        
-        <div className="w-auto userpage" style={{paddingBlock:0,paddingLeft:0}}>
+        <div className="c">
+        <div className="w-auto userpage" style={{paddingBlock:30,paddingLeft:30,paddingRight:30}}>
         <Header/>
         <div className="d-flex align-items-start">
-        <div className="nav sidenav flex-column nav-tabs" style={{width:200,position:"relative"}} id="nav-tab" role="tablist" aria-orientation="vertical">
-            <button className=" navbtn nav-link active" style={{width:200,height:75}} id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
-            <button className="navbtn nav-link " style={{width:200,height:75}} id="nav-applications-tab" data-bs-toggle="tab" data-bs-target="#nav-applications" type="button" role="tab" aria-controls="nav-applications" aria-selected="false">Applications</button>
-            <button className="navbtn nav-link" style={{width:200,height:75}} id="nav-messages-tab" data-bs-toggle="tab" data-bs-target="#nav-messages" type="button" role="tab" aria-controls="nav-messages" aria-selected="false">Review</button>
-            <button className="navbtn nav-link" style={{width:200,height:75}} id="nav-settings-tab" data-bs-toggle="tab" data-bs-target="#nav-settings" type="button" role="tab" aria-controls="nav-settings" aria-selected="false">Dashboard</button>
+        <div className="nav sidenav flex-column nav-tabs" style={{width:300,position:"relative"}} id="nav-tab" role="tablist" aria-orientation="vertical">
+            <button className=" navbtn nav-link active" style={{width:300,height:75}} id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
+            <button className="navbtn nav-link " style={{width:300,height:75}} id="nav-applications-tab" data-bs-toggle="tab" data-bs-target="#nav-applications" type="button" role="tab" aria-controls="nav-applications" aria-selected="false">Applications</button>
+            <button className="navbtn nav-link" style={{width:300,height:75}} id="nav-messages-tab" data-bs-toggle="tab" data-bs-target="#nav-messages" type="button" role="tab" aria-controls="nav-messages" aria-selected="false">Review</button>
+            <button className="navbtn nav-link" style={{width:300,height:75}} id="nav-settings-tab" data-bs-toggle="tab" data-bs-target="#nav-settings" type="button" role="tab" aria-controls="nav-settings" aria-selected="false">Dashboard</button>
         </div>
         
         <div className="container navtabs w-100" style={{paddingLeft:10}}>
@@ -24,6 +24,7 @@ function Navbar() {
             <div className="tab-pane fade" id="nav-applications" role="tabpanel" aria-labelledby="nav-applications-tab"><Application/></div>
             <div className="tab-pane fade" id="nav-messages" role="tabpanel" aria-labelledby="nav-messages-tab">...</div>
             <div className="tab-pane fade" id="nav-settings" role="tabpanel" aria-labelledby="nav-settings-tab">...</div>
+        </div>
         </div>
         </div>
         </div>
